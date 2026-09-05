220f0dcc5b1e59b1a58d8b59b3b9210e407b0429
Massinissa Aidel <aidelmassi@gmail.com>
2026-09-05 16:28:34 +0200

    feat:swagger

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
+
 import { connectDB } from './config/db.js';
 import authRoutes from './routes/auth.routes.js';
 import serviceRoutes from './routes/service.routes.js';
 import bookingRoutes from './routes/booking.routes.js';
+import swaggerUi from 'swagger-ui-express';
+
 dotenv.config();
 
 const app = express();
@@ -17,6 +22,13 @@ app.use(express.json());
 app.use('/api/auth',authRoutes)
 app.use('/api/services', serviceRoutes);
 app.use('/api/bookings', bookingRoutes);
+
+const swaggerFile = JSON.parse(
+  fs.readFileSync(path.resolve('./src/swagger/swagger-output.json'), 'utf-8')
+);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 // Route test
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'API opérationnelle' });
@@ -27,9 +39,9 @@ const startServer = async () => {
     console.log('Tentative de connexion à MongoDB...');
     await connectDB();
     console.log('Connexion réussie, lancement du serveur...');
-
     app.listen(PORT, () => {
       console.log(`Serveur démarré sur http://localhost:${PORT}`);
+      console.log('Swagger dispo sur http://localhost:3000/api-docs');
     });
   } catch (error) {
     console.error('Erreur au lancement du serveur :', error);
---
+++ api/src/swagger/swagger.ts
@@ -0,0 +1,16 @@
+import swaggerAutogen from 'swagger-autogen';
+
+const doc = {
+  info: {
+    title: 'API Services',
+    description: 'Documentation générée automatiquement',
+    version: '1.0.0',
+  },
+  host: 'localhost:3000', 
+  schemes: ['http'],
+};
+
+const outputFile = 'src/swagger/swagger-output.json';
+const endpointsFiles = ['src/server.ts'];
+
+swaggerAutogen({ openapi: '3.0.0' })(outputFile, endpointsFiles, doc);
