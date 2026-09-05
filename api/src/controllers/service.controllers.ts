b793faebfd0dc80d5733900cfedb54e157ada55d
Massinissa Aidel <aidelmassi@gmail.com>
2026-09-05 15:29:38 +0200

    feat:delete update service controller

--- api/src/controllers/service.controllers.ts
+++ api/src/controllers/service.controllers.ts
@@ -18,3 +18,33 @@ export const getServices = async (req: Request, res: Response): Promise<void> =>
     res.status(500).json({ message: 'Erreur récupération services', error });
   }
 };
+
+export const updateService = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const service = await Service.findByIdAndUpdate(id, req.body, { new: true });
+    
+    if (!service) {
+      res.status(404).json({ message: 'Service introuvable' });
+      return;
+    }
+    res.json(service);
+  } catch (error) {
+    res.status(500).json({ message: 'Erreur lors de la mise à jour du service', error });
+  }
+};
+
+export const deleteService = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const service = await Service.findByIdAndDelete(id);
+    
+    if (!service) {
+      res.status(404).json({ message: 'Service introuvable' });
+      return;
+    }
+    res.json({ message: 'Service supprimé avec succès' });
+  } catch (error) {
+    res.status(500).json({ message: 'Erreur lors de la suppression du service', error });
+  }
+};
--- api/src/routes/service.routes.ts
+++ api/src/routes/service.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import { createService, getServices } from '../controllers/service.controllers.js';
+import { createService, getServices,updateService,deleteService } from '../controllers/service.controllers.js';
 import { authenticate } from '../middlewares/auth.js';
 import { authorize } from '../middlewares/requireAdmin.js';
 import { UserRole } from '../models/User.js';
 const router = Router();
 
 router.get('/', authenticate, getServices);
-
 router.post('/', authenticate, authorize(UserRole.ADMIN), createService);
-
+router.put('/:id', authenticate, authorize(UserRole.ADMIN), updateService);
+router.delete('/:id', authenticate, authorize(UserRole.ADMIN), deleteService);
 export default router;
